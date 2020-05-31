cc84e075c5d8e50b455a4439666f821fb13d245d
Fly365 <771645231@qq.com>
2020-05-31 22:34:59 +0800

    极客相关

---
+++ time-geekbang/init-spring/src/test/java/cn/learn/time/geekbang/initspring/InitSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.learn.time.geekbang.initspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
